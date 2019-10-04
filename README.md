eba5699eeb9bc4008f692ff0431d792a39bef559
Desmond Edem <edemintech@gmail.com>
2019-10-04 06:56:09 +0100

    finalize project setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Role-Manager
+
 [![Build Status](https://travis-ci.com/Olamide6774/Role-Manager.svg?branch=develop)](https://travis-ci.com/Olamide6774/Role-Manager)
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import Debug from 'debug';
 import express from 'express';
 import logger from 'morgan';
-import swaggerDoc from './docs/role-manager-api.json';
 import swaggerUi from 'swagger-ui-express';
+import swaggerDoc from '../docs/role-manager-api.json';
 
 const app = express();
 
 if (
-    process.env.NODE_ENV === 'development' ||
-    process.env.NODE_ENV === 'production'
+  process.env.NODE_ENV === 'development'
+    || process.env.NODE_ENV === 'production'
 ) {
-    app.use(logger('dev'));
+  app.use(logger('dev'));
 }
 
 const DEBUG = Debug('dev');
@@ -21,25 +21,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'Welcome to Role-Manager...'
-    });
+  res.status(200).json({
+    status: 'success',
+    message: 'Welcome to Role-Manager...'
+  });
 });
 
 app.get((req, res, next) => {
-    const error = new Error("resource not found");
-    error.status = 404;
-    next()
-})
+  const error = new Error('resource not found');
+  error.status = 404;
+  next();
+});
 
 app.get((error, req, res) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: "an error occured while loading this page"
-        }
-    })
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: 'an error occurred while loading this page'
+    }
+  });
 });
 
 app.listen(PORT, () => DEBUG(`Server running on port ${PORT}`));
